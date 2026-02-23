a4b0792b13135f662cfea947fe4525c4ea457cd9
kolyanhabib <kolyanhabib@gmail.com>
2026-02-23 18:32:06 +0600

    emptystatus hidden and tab button and filter logic completed. now script file has been completed

--- script.js
+++ script.js
@@ -78,3 +78,93 @@ cards.forEach(function (card) {
     filterCards(currentTab);
   });
 });
+
+function filterCards(type) {
+  currentTab = type;
+
+  const allCards = document.querySelectorAll(".job-card");
+
+  let visible = 0;
+  let total = allCards.length;
+
+  allCards.forEach(function (card) {
+    const text = card.querySelector(".status").innerText;
+
+    card.style.display = "none";
+
+    if (type === "ALL") {
+      card.style.display = "block";
+      visible++;
+    }
+
+    if (type === "INTERVIEW" && text === "INTERVIEW") {
+      card.style.display = "block";
+      visible++;
+    }
+
+    if (type === "REJECTED" && text === "REJECTED") {
+      card.style.display = "block";
+      visible++;
+    }
+  });
+
+  if (type === "ALL") {
+    availableJobs.innerText = total + " Jobs";
+  } else {
+    if (visible === 0) {
+      availableJobs.innerText = "0 Jobs";
+    } else {
+      availableJobs.innerText = visible + " of " + total + " Jobs";
+    }
+  }
+
+  checkEmpty();
+}
+
+function checkEmpty() {
+  let visible = 0;
+
+  const allCards = document.querySelectorAll(".job-card");
+
+  allCards.forEach(function (card) {
+    if (card.style.display !== "none") visible++;
+  });
+
+  if (visible === 0) {
+    emptyStatus.classList.remove("hidden");
+  } else {
+    emptyStatus.classList.add("hidden");
+  }
+}
+
+function setActiveTab(activeBtn) {
+  allTab.classList.remove("bg-blue-600", "text-white");
+  interviewTab.classList.remove("bg-blue-600", "text-white");
+  rejectedTab.classList.remove("bg-blue-600", "text-white");
+
+  allTab.classList.add("bg-white", "text-slate-500");
+  interviewTab.classList.add("bg-white", "text-slate-500");
+  rejectedTab.classList.add("bg-white", "text-slate-500");
+
+  activeBtn.classList.remove("bg-white", "text-slate-500");
+  activeBtn.classList.add("bg-blue-600", "text-white");
+}
+
+allTab.addEventListener("click", function () {
+  filterCards("ALL");
+  setActiveTab(allTab);
+});
+
+interviewTab.addEventListener("click", function () {
+  filterCards("INTERVIEW");
+  setActiveTab(interviewTab);
+});
+
+rejectedTab.addEventListener("click", function () {
+  filterCards("REJECTED");
+  setActiveTab(rejectedTab);
+});
+
+updateCounts();
+filterCards("ALL");
+setActiveTab(allTab);
